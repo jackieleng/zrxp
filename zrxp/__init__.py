4b2bdae1edd075cf7b1a533df9ccccafaf023a98
Jackie Leng <jccleng@gmail.com>
2019-06-08 23:47:12 +0200

    first commit, finished POC

---
+++ zrxp/__init__.py
@@ -0,0 +1,68 @@
+from parsimonious import Grammar, NodeVisitor
+
+
+# TODO: comments
+grammar = Grammar(
+    r"""
+    zrxp = single_timeseries+
+    single_timeseries = metadata_headers records
+
+    metadata_headers = metadata_header+
+    records = record+
+
+    metadata_header = hashtag (metadata_field metadata_sep)+ ws+
+    hashtag = "#"
+    metadata_field = ~r"[-\w\.\(\)\,\-\s]+"
+    metadata_sep = "|*|"
+
+    record = field (field_sep field)* ws+
+    field = ~r"[-\w\d\.]+"
+    field_sep = ~r"\s+"
+
+    comment = ~r"##.*"
+    ws = ~"\s*"
+    """
+)
+
+
+class ZRXPVisitor(NodeVisitor):
+    def visit_zrxp(self, node, visited_children):
+        return visited_children
+
+    def visit_single_timeseries(self, node, visited_children):
+        metadata, records = visited_children
+        output = {"metadata": metadata, "records": records}
+        return output
+
+    def visit_metadata_headers(self, node, visited_children):
+        flat_list = [item for sublist in visited_children for item in sublist]
+        return flat_list
+
+    def visit_metadata_header(self, node, visited_children):
+        _, metadata, _ = visited_children
+        return [field for (field, sep) in metadata]
+
+    def visit_metadata_field(self, node, visited_children):
+        print(node.text)
+        return node.text
+
+    def generic_visit(self, node, visited_children):
+        return visited_children or node
+
+
+zv = ZRXPVisitor()
+with open("05BJ004.HG.datum.O.zrx", "r") as f:
+    s = f.read()
+    tree = grammar.parse(s)
+    print(tree)
+
+    output = zv.visit(tree)
+    print(output)
+
+with open("multi_ts.zrx", "r") as f:
+    s = f.read()
+    tree_multi = grammar.parse(s)
+    print(tree_multi)
+
+    output_multi = zv.visit(tree)
+    print(output_multi)
